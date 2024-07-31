eed441fa05c8142e147d402b1374b310a5b3e2c5
tukainpng <dfern3321@gmail.com>
2024-07-31 20:44:22 -0300

    Updates

--- lua/core/config/lsp.lua
+++ lua/core/config/lsp.lua
@@ -4,7 +4,6 @@ require("lspconfig").lua_ls.setup({})
 require("lspconfig").cssls.setup({})
 require("lspconfig").html.setup({})
 require("lspconfig").tsserver.setup({})
-require("lspconfig").astro.setup({})
 
 require("cmp").setup { sources = { { name = 'nvim_lsp' } } }
 local capabilities = require("cmp_nvim_lsp").default_capabilities()
@@ -14,5 +13,4 @@ require'lspconfig'.cssls.setup { capabilities = capabilities, }
 require'lspconfig'.html.setup { capabilities = capabilities, }
 require'lspconfig'.tsserver.setup { capabilities = capabilities, }
 require'lspconfig'.clangd.setup { capabilities = capabilities, }
-require'lspconfig'.astro.setup { capabilities = capabilities, }
 
